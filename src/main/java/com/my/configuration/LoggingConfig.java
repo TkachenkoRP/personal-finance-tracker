3ac05352108a9961df8aa14299e5591046c5347f
Rodion Tkachenko <tkachenko_rp@mail.ru>
2025-04-04 02:17:32 +0700

    добавлен собственный стартер для логирования

---
+++ src/main/java/com/my/configuration/LoggingConfig.java
@@ -0,0 +1,9 @@
+package com.my.configuration;
+
+import com.my.myauditloggingstarter.annotation.EnableLoggingAspect;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableLoggingAspect
+public class LoggingConfig {
+}
